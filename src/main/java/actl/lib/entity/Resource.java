ec4fcf5d5bf6f5bffae52f99cd800a81400c55c0
Felipe Curty <felipecrp@gmail.com>
2022-05-15 19:28:52 -0300

    Use JUnit 5

--- src/main/java/actl/lib/entity/Resource.java
+++ src/main/java/actl/lib/entity/Resource.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 @EqualsAndHashCode
-public class Resource {
+public abstract class Resource {
     @Getter @Setter
     private String name;
 
--- src/test/java/actl/lib/AccessManagerTest.java
+++ src/test/java/actl/lib/AccessManagerTest.java
@@ -1,48 +1,53 @@
 package actl.lib;
 
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 
 import actl.lib.entity.Access;
 import actl.lib.entity.Repository;
 import actl.lib.entity.Role;
 import actl.lib.entity.User;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class AccessManagerTest {
-    
+    private Repository repositoryA;
+    private Repository repositoryB;
+    private User user;
+
     @BeforeEach
     public void setup() {
-        Repository repositoryA = new Repository("repository_a");
-        Repository repositoryB = new Repository("repository_b");
-        User user = new User(repositoryA, "user");
-        user.getAccesses().add(new Access(user, new Role(repositoryA, "role")));
-        user.getAccesses().add(new Access(user, new Role(repositoryB, "role")));
+        this.repositoryA = new Repository("repository_a");
+        this.repositoryB = new Repository("repository_b");
+        this.user = new User(repositoryA, "user");
+        this.user.getAccesses().add(
+            new Access(this.user, new Role(repositoryA, "role_a")));
+        this.user.getAccesses().add(
+            new Access(this.user, new Role(repositoryB, "role_a")));
     }
 
     @Test
     public void it_grants_access() {
-        Repository repository = new Repository("repository");
-        User user = new User(repository, "user");
-        Role role = new Role(repository, "role");
+        Role role = new Role(this.repositoryA, "role_b");
 
         AccessManager accessManager = new AccessManager();
-        accessManager.grant(user, role);
+        accessManager.grant(this.user, role);
 
-        Access access = new Access(user, role);
+        Access access = new Access(this.user, role);
         assert user.getAccesses().contains(access);
     }
 
     @Test
+    @DisplayName("It revokes access")
     public void it_revokes_access() {
-        Repository repository = new Repository("repository");
-        User user = new User(repository, "user");
-        Role role = new Role(repository, "role");
-        Access access = new Access(user, role);
-        user.getAccesses().add(access);
+        Role role = new Role(this.repositoryA, "role_a");
 
         AccessManager accessManager = new AccessManager();
-        accessManager.revoke(user, role);
+        accessManager.revoke(this.user, role);
 
+        Access access = new Access(user, role);
         assert !user.getAccesses().contains(access);
     }
 }
